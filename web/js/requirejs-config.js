23c40977e769aa061d60733efc0335759e6ddf50
Kegan VanSickle <keganv@keganv.com>
2015-11-02 19:13:09 -0600

    Setting up requirejs configuration.

---
+++ web/js/requirejs-config.js
@@ -0,0 +1,24 @@
+requirejs.config({
+    baseUrl: "/",
+    paths: {
+        "jquery": [
+            "//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min",
+            "vendor/jquery/dist/jquery.min"
+        ],
+        "bootstrap": [
+            "//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min",
+            "vendor/bootstrap/dist/js/bootstrap.min"
+        ],
+        "angular": [
+            "//ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular.min.js",
+            "vendor/angular/angular.min"
+        ],
+        "main": "js/main"
+    },
+    shim: {
+        "bootstrap": {
+            deps: ["jquery"]
+        }
+    },
+    waitSeconds: 10
+});
