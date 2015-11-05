822cb3890430e38779313fa38c75499aa00ded03
Kegan VanSickle <keganv@keganv.com>
2015-11-04 21:18:35 -0600

    Added logic for setting the victor and added it to the form view.

--- src/AppBundle/Form/Subscriber/BattleTypeSubscriber.php
+++ src/AppBundle/Form/Subscriber/BattleTypeSubscriber.php
@@ -23,11 +23,45 @@ class BattleTypeSubscriber implements EventSubscriberInterface
     {
         $battle = $event->getData();
 
-        if (isset($battle['computerWeapon'])) {
-            var_dump($battle);
-            die($battle['computerWeapon']);
+        if (isset($battle['computerWeapon']) && isset($battle['victor'])) {
+            $battle['computerWeapon'] = rand(0, 4);
+            $battle['victor'] = $this->setVictor($battle);
         }
 
         $event->setData($battle);
+    }
+
+    /**
+     * Sets the victor of the battle
+     * 
+     * @param $battle
+     * @return int
+     */
+    private function setVictor($battle)
+    {
+        $uWeapon = $battle['userWeapon'];
+        $comWeapon = $battle['computerWeapon'];
+        return $this->determineVictor($uWeapon, $comWeapon);
+    }
+
+    /**
+     * Determines the victor of the battle
+     * Possible values are: 0 = player wins, 1 = computer wins, 2 = Tie
+     *
+     * @param int $u
+     * @param int $c
+     * @return int $result
+     */
+    private function determineVictor($u, $c)
+    {
+        $victor = (5 + ((int) $u - (int) $c)) % 5;
+
+        if ($victor == 1 || $victor == 3) {
+            return 0;
+        } elseif ($victor == 2 || $victor == 4) {
+            return 1;
+        }
+
+        return 2;
     }
 }
